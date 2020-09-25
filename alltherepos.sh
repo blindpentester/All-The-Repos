57951ea51763e55cfbfca8bf1260f10a77d34544
blindpentester <71842000+blindpentester@users.noreply.github.com>
2020-09-25 09:14:27 -0600

    Create alltherepos.sh

---
+++ alltherepos.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+# A script to cd into every /opt repo and perform a pull to make sure they are updated
+# Git has recently made this a bit more difficult so I wanted to make this an easier
+# process for myself and anyone else interested.
+
+for d in /opt/*; do (cd "$d" && git config core.fileMode false && git config pull.rebase false && git pull); done
