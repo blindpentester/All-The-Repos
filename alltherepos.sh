016441094abec5924174a45abf4ef6273878f015
blindpentester <71842000+blindpentester@users.noreply.github.com>
2020-09-25 09:22:35 -0600

    Update alltherepos.sh

--- alltherepos.sh
+++ alltherepos.sh
@@ -4,4 +4,4 @@
 # Git has recently made this a bit more difficult so I wanted to make this an easier
 # process for myself and anyone else interested.
 
-for d in /opt/*; do (cd "$d" && git config core.fileMode false && git config pull.rebase false && git pull); done
+for d in /opt/*; do (cd "$d" && git config core.fileMode false && git config pull.rebase false && git pull); done 2> /dev/null
